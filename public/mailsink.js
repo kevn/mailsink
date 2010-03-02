d3b78c71a9f23a8a4e2a340fe1588ce5faddb750
Kevin E. Hunt <kevin@kev.in>
2010-03-01 19:19:56 -0800

    Hide headers immediately

--- public/mailsink.js
+++ public/mailsink.js
@@ -10,7 +10,7 @@ headers = {
   },
   
   hide: function() {
-      $(".headers").hide('fast');
+      $(".headers").hide();
       $("#header-control").replaceWith(
           $('<h2 id="header-control">Show Headers</h2>').click(function() {
               headers.show();
